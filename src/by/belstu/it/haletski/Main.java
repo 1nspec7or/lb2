95a8e161b0178a52ce780d3ae8f644773d9424ae
Anton Haletski <anton12032000@gmail.com>
2021-02-15 11:24:09 +0300

    Add to do message

---
+++ src/by/belstu/it/haletski/Main.java
@@ -0,0 +1,20 @@
+package by.belstu.it.haletski;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        FirstProject obj = new FirstProject();
+
+        message();
+
+        for(int i=0; i<10; i++){System.out.println();}
+        for (int a = 0; a < 5; a++) {
+            System.out.println(a);
+        }
+    }
+
+    private static void message() {
+        System.out.println("Message");
+    }
+}
